38440ce173a6e3fc2ea6e76978187bab31420a56
Ecwy <2461864879@qq.com>
2019-06-30 10:32:23 +0800

    Site updated: 2019-06-30 10:32:22

---
+++ js/count.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?4b3048feed996ba1132ec5ce274160ab";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
